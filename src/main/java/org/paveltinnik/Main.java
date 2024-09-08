41d93404129e9dc112f3d74a61b2891672108e19
Павел <t280396ab@mail.ru>
2024-09-08 22:22:46 +0700

    refactor

--- src/main/java/org/paveltinnik/Main.java
+++ src/main/java/org/paveltinnik/Main.java
@@ -1,21 +1,6 @@
 package org.paveltinnik;
 
-import java.util.ArrayList;
-import java.util.List;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(5);
-        MyArrayList<String> myArrayList = new MyArrayList<>();
-        myArrayList.add("asdf");
-        myArrayList.add("uioyui");
-        myArrayList.add("gf");
-        myArrayList.add("jilkh");
-        System.out.println("myArrayList.size() = " + myArrayList.size());
-        System.out.println("myArrayList.get(0) = " + myArrayList.get(0));
-        myArrayList.quickSort();
-        System.out.println("myArrayList.quickSort() = " + myArrayList);
     }
 }
